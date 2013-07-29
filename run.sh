93721a66d57cfc57aef3b737f7780a98bc05230a
Jacco Flenter <github@putpixel.nl>
2013-07-29 22:29:57 +0200

    no longer use sudo

--- run.sh
+++ run.sh
@@ -2,7 +2,7 @@ cd $WERCKER_SOURCE_DIR
 
 if [ -f "requirements.txt" ]
 then
-    debug "sudo pip install -r requirements.txt"
+    debug "pip install -r requirements.txt"
     sudo pip install -r requirements.txt
 
     if [[ $? -ne 0 ]]
