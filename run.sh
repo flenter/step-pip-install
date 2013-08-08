38210455a6b1c5fb2defb2caed760f5143a91033
Jacco Flenter <github@putpixel.nl>
2013-08-08 10:44:01 +0200

    upgrade pip

--- run.sh
+++ run.sh
@@ -3,8 +3,12 @@ cd $WERCKER_SOURCE_DIR
 if [ -f "requirements.txt" ]
 then
     debug "pip install -r requirements.txt"
+    pip --version
+    pip install --no-install --use-mirrors -I --download=$WERCKER_CACHE_DIR pip
+    pip install --no-install --use-mirrors -I --download=$WERCKER_CACHE_DIR pip --upgrade
+    pip --version
     pip install --no-install --use-mirrors -I --download=$WERCKER_CACHE_DIR -r requirements.txt
-    sudo pip install --find-links=file://$WERCKER_CACHE_DIR --no-index --index-url=file:///dev/null -r requirements.txt
+    sudo pip install --find-links=file://$WERCKER_CACHE_DIR --no-index --use-wheel --index-url=file:///dev/null -r requirements.txt
 
     if [[ $? -ne 0 ]]
     then
