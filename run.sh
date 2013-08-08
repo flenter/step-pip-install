4d88553a5e72c44711553acb2c6c87b6c088c1f0
Jacco Flenter <github@putpixel.nl>
2013-08-08 14:23:08 +0200

    use SOURCE_DIR and CACHE_DIR (+bump)

--- run.sh
+++ run.sh
@@ -3,11 +3,11 @@ cd $WERCKER_SOURCE_DIR
 if [ -f "requirements.txt" ]
 then
     debug "install pip-accell and use it to install packages from requirements.txt"
-    ls -la $WERCKER_SOURCE_DIR
-    sudo pip install --download-cache=$WERCKER_SOURCE_DIR pip --upgrade
-    sudo pip install --download-cache=$WERCKER_SOURCE_DIR setuptools --upgrade
-    sudo pip install --download-cache=$WERCKER_SOURCE_DIR wheel
-    sudo pip install --download-cache=$WERCKER_SOURCE_DIR --use-wheel -r requirements.txt 
+    ls -la $WERCKER_CACHE_DIR
+    sudo pip install --download-cache=$WERCKER_CACHE_DIR pip --upgrade
+    sudo pip install --download-cache=$WERCKER_CACHE_DIR setuptools --upgrade
+    sudo pip install --download-cache=$WERCKER_CACHE_DIR wheel
+    sudo pip install --download-cache=$WERCKER_CACHE_DIR --use-wheel -r requirements.txt 
 
     if [[ $? -ne 0 ]]
     then
