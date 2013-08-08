f5276a4d68d94dc9d6e21e654060e64d9b21230b
Jacco Flenter <github@putpixel.nl>
2013-08-08 14:54:51 +0200

    back to simple

--- run.sh
+++ run.sh
@@ -2,13 +2,9 @@ cd $WERCKER_SOURCE_DIR
 
 if [ -f "requirements.txt" ]
 then
-    debug "install pip-accell and use it to install packages from requirements.txt"
+    debug "pip install with download cache"
     ls -la $WERCKER_CACHE_DIR
-    sudo pip install --download-cache=$WERCKER_CACHE_DIR pip --upgrade
-    sudo pip install --download-cache=$WERCKER_CACHE_DIR setuptools --upgrade
-    sudo pip install --download-cache=$WERCKER_CACHE_DIR wheel
-    sudo pip freeze
-    sudo pip install --download-cache=$WERCKER_CACHE_DIR --use-wheel -r requirements.txt 
+    sudo pip install --download-cache=$WERCKER_CACHE_DIR -r requirements.txt 
 
     if [[ $? -ne 0 ]]
     then
