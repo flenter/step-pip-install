5ba99e313889633edec55196dde7902bb259b669
Jacco Flenter <github@putpixel.nl>
2013-08-08 10:03:05 +0200

    the hard way: split pip install into two steps.
    see:
    http://stackoverflow.com/questions/4806448/how-do-i-install-from-a-local-cache-with-pip

--- run.sh
+++ run.sh
@@ -3,7 +3,8 @@ cd $WERCKER_SOURCE_DIR
 if [ -f "requirements.txt" ]
 then
     debug "pip install -r requirements.txt"
-    sudo pip install -r requirements.txt
+    pip install --no-install --use-mirrors -I --download=$WERCKER_CACHE_DIR -r requirements.txt
+    sudo pip install --find-links=file://$WERCKER_CACHE_DIR --no-index --index-url=file:///dev/null <package name>
 
     if [[ $? -ne 0 ]]
     then
